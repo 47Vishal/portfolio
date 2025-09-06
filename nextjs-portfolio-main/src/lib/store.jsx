6db9c0fddd2e87675c44fefa6630f8c4ade38da8
47vishal <vishaltodsam55@gmail.com>
2025-09-06 02:23:20 -0700

    frontend

--- nextjs-portfolio-main/src/lib/store.jsx
+++ nextjs-portfolio-main/src/lib/store.jsx
@@ -17,6 +17,7 @@ export const store = configureStore({
     getDefaultMiddleware().concat(userAuthAPI.middleware),
 })
 
+
 // optional, but required for refetchOnFocus/refetchOnReconnect behaviors
 // see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
 setupListeners(store.dispatch)
